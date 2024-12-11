2c7c580f4a4467af57acfbfb3769ed14e5466cbd
Machine account 05 <dick.visser+machine05@geant.org>
2024-12-11 16:36:24 +0100

    remove temp files

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-. ~/venv/bin/activate
-
-cat ~/foo/metadata.xml |\
-    xq '."md:EntitiesDescriptor"."md:EntityDescriptor"[]."md:ContactPerson" |= sort_by(."@contactType",."md:EmailAddress",."md:SurName",."md:GivenName")' |\
-    yq -r .
-
